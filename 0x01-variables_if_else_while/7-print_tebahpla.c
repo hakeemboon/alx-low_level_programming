762dfcc3743f3ebf5b2101cae906857404fa185b
hakeemboon <abdihakiimboon@gmail.com>
2023-02-17 13:12:22 -0800

    revers alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char n;
+
+	n = 'z';
+	while
+		(n >= 'a')
+		{
+			putchar(n);
+			n--;
+		}
+	putchar('\n');
+	return (0);
+}
