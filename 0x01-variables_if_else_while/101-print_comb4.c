a62f3204dbdaee01ced409d3293127415723b1c9
hakeemboon <abdihakiimboon@gmail.com>
2023-02-17 14:23:49 -0800

    a

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 /**
  * main - Entry point
  *
@@ -6,34 +7,34 @@
  */
 int main(void)
 {
-	int a;
-	int b;
-	int c = 0;
+	int c;
+	int d;
+	int e = 0;
 
-	while (c < 10)
+	while (e < 10)
 	{
-		b = 0;
-		while (b < 10)
+		c = 0;
+		while (c < 10)
 		{
-			a = 0;
-			while (a < 10)
+			e = 0;
+			while (e < 10)
 			{
-				if (a != b && b != c && c != b && b != a)
+				if (c != d && d != e && e != d && d != c)
 				{
+					putchar('0' + e);
+					putchar('0' + d);
 					putchar('0' + c);
-					putchar('0' + b);
-					putchar('0' + a);
-					if (a + b + c != 9 + 8 + 7)
+					if (c + d + e != 9 + 8 + 7)
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
-				a++;
+				c++;
 			}
-			b++;
+			d++;
 		}
-		c++;
+		e++;
 	}
 	putchar('\n');
 	return (0);
