2e26babb6ff51874c19363f170aacd1062cb6e35
hakeemboon <abdihakiimboon@gmail.com>
2023-02-17 13:34:46 -0800

    combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,8 +12,10 @@ int main(void)
 	for (a = 0; a < 100; a++)
 	{
 		putchar(a + '0');
-		putchar(',');
-		putchar(' ');
+		if (a < 99) {
+         		putchar(',');
+	        	putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
